62069103eab6eba3779d68f533f2bf8755c9dc72
Varshini <94217045+varshinir07@users.noreply.github.com>
2025-05-31 11:39:51 +0530

    Simple weather bot

---
+++ google-adk-weather-bot/src/test/java/com/adk/googleadkweatherbot/GoogleAdkWeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adk.googleadkweatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAdkWeatherBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
